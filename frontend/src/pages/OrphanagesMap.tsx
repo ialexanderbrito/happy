6ca6032eb54b6097fa7ad78aa24ee279a843ba9a
Alexander <xanderzinho26@gmail.com>
2020-10-12 17:48:27 -0300

    :pencil2: Bug fix

--- frontend/src/pages/OrphanagesMap.tsx
+++ frontend/src/pages/OrphanagesMap.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 import {
   PageMapContainer,
@@ -33,7 +35,15 @@ function OrphanagesMap() {
         </Footer>
       </Aside>
 
-      <div></div>
+      <Map
+        center={[-22.7671747, -43.3347008]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
 
       <CreateOrphanage to="">
         <FiPlus size={32} color="#FFF" />
