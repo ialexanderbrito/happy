1fd24a09c56cc249bee97da9d4f2081390fef02b
Alexander <xanderzinho26@gmail.com>
2020-10-13 19:41:38 -0300

    :sparkles: Lidando com exceções e erros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 
 import './database/connection';
 
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.use(errorHandler);
 
 app.listen(3333, () => {
   console.log('🚀 Backend started!');
