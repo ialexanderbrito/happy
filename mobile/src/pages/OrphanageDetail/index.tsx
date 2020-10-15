3dc5801d0a6d966275eace2b86b612245d593d62
Alexander <xanderzinho26@gmail.com>
2020-10-15 15:24:07 -0300

    :sparkles: Criado página de Detalhe

---
+++ mobile/src/pages/OrphanageDetail/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const OrphanageDetail: React.FC = () => {
+  return (
+    <Container>
+      <Text>OrphanageDetail</Text>
+    </Container>
+  );
+};
+
+export default OrphanageDetail;
