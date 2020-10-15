f87106c1d5330e51f31a779a918ea1a607e4e876
Alexander <xanderzinho26@gmail.com>
2020-10-15 15:27:04 -0300

    :sparkles: Adicionado navegação de tela

--- mobile/src/pages/OrphanagesMap/index.tsx
+++ mobile/src/pages/OrphanagesMap/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
 
@@ -15,6 +16,11 @@ import {
 } from './styles';
 
 const OrphanagesMap: React.FC = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToOrphanageDetail() {
+    navigation.navigate('OrphanageDetail');
+  }
   return (
     <Wrapper>
       <Map
@@ -34,7 +40,7 @@ const OrphanagesMap: React.FC = () => {
           icon={mapMarker}
           coordinate={{ latitude: -22.767972, longitude: -43.3349147 }}
         >
-          <Callout tooltip onPress={() => {}}>
+          <Callout tooltip onPress={handleNavigateToOrphanageDetail}>
             <CalloutContainer>
               <CalloutText>Lar das crianças</CalloutText>
             </CalloutContainer>
