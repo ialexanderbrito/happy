65c8429cff4ee0f6771483e1cb4848b9170ea7df
Alexander <xanderzinho26@gmail.com>
2020-10-16 15:30:34 -0300

    :sparkles: Criado página Criação de Orfanato

---
+++ mobile/src/pages/OrphanageData/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const OrphanageData: React.FC = () => {
+  return (
+    <Container>
+      <Text>OrphanageData</Text>
+    </Container>
+  );
+};
+
+export default OrphanageData;
