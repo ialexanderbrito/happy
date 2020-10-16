4ee92f1dde6cd601d16d523992df82891bd8e935
Alexander <xanderzinho26@gmail.com>
2020-10-16 17:39:20 -0300

    :sparkles: Adicionado página de Detalhe do Orfanato

--- mobile/src/pages/OrphanageDetail/index.tsx
+++ mobile/src/pages/OrphanageDetail/index.tsx
@@ -1,12 +1,122 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import { Feather, FontAwesome } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
 
-import { Container } from './styles';
+import darkMap from '../../themes/darkMap.json';
+
+import mapMarker from '../../assets/images/map-marker.png';
+
+import {
+  Container,
+  ImagesContainer,
+  Img,
+  PagingEnabled,
+  DetailsContainer,
+  Title,
+  Description,
+  MapContainer,
+  Map,
+  RoutesContainer,
+  RoutesText,
+  Separator,
+  ScheduleContainer,
+  ScheduleItemBlue,
+  ScheduleTextBlue,
+  ScheduleItemGreen,
+  ScheduleTextGreen,
+  ScheduleItemRed,
+  ScheduleTextRed,
+  ContactButton,
+  ContactButtonText,
+} from './styles';
 
 const OrphanageDetail: React.FC = () => {
+  const theme = useTheme();
+
   return (
     <Container>
-      <Text>OrphanageDetail</Text>
+      <ImagesContainer>
+        <PagingEnabled horizontal pagingEnabled>
+          <Img
+            resizeMode="cover"
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg',
+            }}
+          />
+          <Img
+            resizeMode="cover"
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg',
+            }}
+          />
+          <Img
+            resizeMode="cover"
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg',
+            }}
+          />
+        </PagingEnabled>
+      </ImagesContainer>
+
+      <DetailsContainer>
+        <Title>Orfanato Esperança</Title>
+        <Description>
+          Presta assistência a crianças de 06 a 15 anos que se encontre em
+          situação de risco e/ou vulnerabilidade social.
+        </Description>
+
+        <MapContainer>
+          <Map
+            customMapStyle={theme.isLighten ? [] : darkMap}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+              latitude: -22.767972,
+              longitude: -43.3349147,
+              latitudeDelta: 0.008,
+              longitudeDelta: 0.008,
+            }}
+            zoomEnabled={false}
+            pitchEnabled={false}
+            scrollEnabled={false}
+            rotateEnabled={false}
+          >
+            <Marker
+              icon={mapMarker}
+              coordinate={{ latitude: -22.767972, longitude: -43.3349147 }}
+            />
+          </Map>
+
+          <RoutesContainer>
+            <RoutesText>Ver rotas no Google Maps</RoutesText>
+          </RoutesContainer>
+        </MapContainer>
+
+        <Separator />
+
+        <Title>Instruções para visita</Title>
+        <Description>
+          Venha como se sentir a vontade e traga muito amor e paciência para
+          dar.
+        </Description>
+
+        <ScheduleContainer>
+          <ScheduleItemBlue>
+            <Feather name="clock" size={40} color="#2ab5d1" />
+            <ScheduleTextBlue>Segunda à Sexta 8h às 18h</ScheduleTextBlue>
+          </ScheduleItemBlue>
+
+          <ScheduleItemGreen>
+            <Feather name="info" size={40} color="#39cc83" />
+            <ScheduleTextGreen>Atendemos fim de semana</ScheduleTextGreen>
+          </ScheduleItemGreen>
+        </ScheduleContainer>
+
+        <ContactButton onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={24} color="#FFF" />
+          <ContactButtonText>Entrar em contato</ContactButtonText>
+        </ContactButton>
+      </DetailsContainer>
     </Container>
   );
 };
--- mobile/src/pages/OrphanageDetail/styles.ts
+++ mobile/src/pages/OrphanageDetail/styles.ts
@@ -1,5 +1,151 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import MapView from 'react-native-maps';
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
+  flex: 1;
+`;
 
+export const ImagesContainer = styled.View`
+  height: 240px;
+`;
+
+export const Img = styled.Image`
+  width: ${Dimensions.get('window').width};
+  height: 240px;
+`;
+
+export const PagingEnabled = styled.ScrollView``;
+
+export const DetailsContainer = styled.View`
+  padding: 24px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.textSecondary};
+  font-size: 30px;
+  font-family: 'Nunito_700Bold';
+`;
+
+export const Description = styled.Text`
+  font-family: 'Nunito_600SemiBold';
+  line-height: 24px;
+  margin-top: 16px;
+  color: ${({ theme }) => theme.textDescription};
+`;
+
+export const MapContainer = styled.View`
+  border-radius: 20px;
+  overflow: hidden;
+  border-width: 1.2px;
+  border-color: ${({ theme }) => theme.headerBorder};
+  margin-top: 40px;
+  background-color: #e6f7fb;
+`;
+
+export const Map = styled(MapView)`
+  width: 100%;
+  height: 150px;
+`;
+
+export const RoutesContainer = styled.View`
+  padding: 16px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const RoutesText = styled.Text`
+  font-family: 'Nunito_700Bold';
+  color: ${({ theme }) => theme.secondary};
+`;
+
+export const Separator = styled.View`
+  height: 0.8px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.headerColor};
+  margin: 40px 0px;
+`;
+
+export const ScheduleContainer = styled.View`
+  margin-top: 24px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const ScheduleItemBlue = styled.View`
+  width: 48%;
+  padding: 20px;
+
+  background: ${({ theme }) => (theme.isLighten ? '#e6f7fb' : '#104b57')};
+
+  border: 1px;
+  border-color: ${({ theme }) => (theme.isLighten ? '#b3dae2' : '#104b57')};
+  border-radius: 20px;
+`;
+
+export const ScheduleTextBlue = styled.Text`
+  font-family: 'Nunito_600SemiBold';
+  font-size: 16px;
+  line-height: 24px;
+  margin-top: 20px;
+
+  color: #5c8599;
+`;
+
+export const ScheduleItemGreen = styled.View`
+  width: 48%;
+  padding: 20px;
+
+  background: ${({ theme }) => (theme.isLighten ? '#EDFFF6' : '#0b3b23')};
+
+  border: 1px;
+  border-color: ${({ theme }) => (theme.isLighten ? '#A1E9C5' : '#0b3b23')};
+  border-radius: 20px;
+`;
+
+export const ScheduleTextGreen = styled.Text`
+  font-family: 'Nunito_600SemiBold';
+  font-size: 16px;
+  line-height: 24px;
+  margin-top: 20px;
+
+  color: #37c77f;
+`;
+
+export const ScheduleItemRed = styled.View`
+  width: 48%;
+  padding: 20px;
+
+  background: ${({ theme }) => (theme.isLighten ? '#FEF6F9' : '#5e132e')};
+
+  border: 1px;
+  border-color: ${({ theme }) => (theme.isLighten ? '#FFBCD4' : '#5e132e')};
+  border-radius: 20px;
+`;
+
+export const ScheduleTextRed = styled.Text`
+  font-family: 'Nunito_600SemiBold';
+  font-size: 16px;
+  line-height: 24px;
+  margin-top: 20px;
+
+  color: #ff669d;
+`;
+
+export const ContactButton = styled(RectButton)`
+  background-color: ${({ theme }) => theme.tertiary};
+  border-radius: 20px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 56px;
+  margin-top: 40px;
+`;
+
+export const ContactButtonText = styled.Text`
+  font-family: 'Nunito_800ExtraBold';
+  color: #fff;
+  font-size: 16px;
+  margin-left: 16px;
 `;
