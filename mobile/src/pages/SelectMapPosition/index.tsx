76d9594eb9bea41646070e637387fbcd8a018e3a
Alexander <xanderzinho26@gmail.com>
2020-10-16 15:29:55 -0300

    :sparkles: Criado página Selecionar Mapa

---
+++ mobile/src/pages/SelectMapPosition/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SelectMapPosition: React.FC = () => {
+  return (
+    <Container>
+      <Text>SelectMapPosition</Text>
+    </Container>
+  );
+};
+
+export default SelectMapPosition;
