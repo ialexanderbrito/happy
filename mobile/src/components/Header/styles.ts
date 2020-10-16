e5a3d6c42c57453b2b5ccc151293820a6f8d7580
Alexander <xanderzinho26@gmail.com>
2020-10-16 16:10:18 -0300

    :sparkles: Criado componente Header

---
+++ mobile/src/components/Header/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  padding: 24px;
+  background-color: ${({ theme }) => theme.headerColor};
+  border-bottom-width: 1px;
+  border-color: ${({ theme }) => theme.headerBorder};
+  padding-top: 44px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: 'Nunito_600SemiBold';
+  color: ${({ theme }) => theme.textSecondary};
+  font-size: 16px;
+`;
+
+export const Button = styled(BorderlessButton)``;
