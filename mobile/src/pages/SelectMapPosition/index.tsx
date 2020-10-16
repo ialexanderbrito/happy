7556571a07095d1b666d31fcdebe04efc5c9a102
Alexander <xanderzinho26@gmail.com>
2020-10-16 17:58:45 -0300

    :sparkles: Adicionado página de Seleção no Mapa

--- mobile/src/pages/SelectMapPosition/index.tsx
+++ mobile/src/pages/SelectMapPosition/index.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import { useTheme } from 'styled-components';
 
-import { Container } from './styles';
+import darkMap from '../../themes/darkMap.json';
+
+import mapMarker from '../../assets/images/map-marker.png';
+
+import { Container, Map, NextButton, NextButtonText } from './styles';
 
 const SelectMapPosition: React.FC = () => {
+  const theme = useTheme();
+  const { navigate } = useNavigation();
+
+  function handleNextStep() {
+    navigate('OrphanageData');
+  }
+
   return (
     <Container>
-      <Text>SelectMapPosition</Text>
+      <Map
+        customMapStyle={theme.isLighten ? [] : darkMap}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -22.767972,
+          longitude: -43.3349147,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          coordinate={{ latitude: -22.767972, longitude: -43.3349147 }}
+        />
+      </Map>
+
+      <NextButton onPress={handleNextStep}>
+        <NextButtonText>Próximo</NextButtonText>
+      </NextButton>
     </Container>
   );
 };
--- mobile/src/pages/SelectMapPosition/styles.ts
+++ mobile/src/pages/SelectMapPosition/styles.ts
@@ -1,5 +1,33 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import MapView from 'react-native-maps';
 
 export const Container = styled.View`
+  flex: 1;
+  position: relative;
+`;
 
+export const Map = styled(MapView)`
+  width: ${Dimensions.get('window').width};
+  height: ${Dimensions.get('window').height};
+`;
+
+export const NextButton = styled(RectButton)`
+  background-color: ${({ theme }) => theme.secondary};
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
+  height: 56px;
+
+  position: absolute;
+  left: 24px;
+  right: 24px;
+  bottom: 40px;
+`;
+
+export const NextButtonText = styled.Text`
+  font-family: 'Nunito_800ExtraBold';
+  font-size: 16px;
+  color: #fff;
 `;
