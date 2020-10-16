04aabcf9c9ca8074008065914c66c434281192ec
Alexander <xanderzinho26@gmail.com>
2020-10-16 18:28:55 -0300

    :sparkles: Adicionado página de Dados para cadastro

--- mobile/src/pages/OrphanageData/index.tsx
+++ mobile/src/pages/OrphanageData/index.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Label,
+  Input,
+  ImagesInput,
+  SwitchContainer,
+  SwitchChange,
+  NextButton,
+  NextButtonText,
+} from './styles';
 
 const OrphanageData: React.FC = () => {
   return (
-    <Container>
-      <Text>OrphanageData</Text>
+    <Container contentContainerStyle={{ padding: 24 }}>
+      <Title>Dados</Title>
+
+      <Label>Nome</Label>
+      <Input />
+
+      <Label>Sobre</Label>
+      <Input style={{ height: 110 }} multiline />
+
+      <Label>WhatsApp</Label>
+      <Input />
+
+      <Label>Fotos</Label>
+
+      <ImagesInput onPress={() => {}}>
+        <Feather name="plus" size={24} color="#15B6D6" />
+      </ImagesInput>
+
+      <Title>Visitação</Title>
+
+      <Label>Instruções</Label>
+      <Input style={{ height: 110 }} multiline />
+
+      <Label>Horário de visitas</Label>
+      <Input />
+
+      <SwitchContainer>
+        <Label>Atende final de semana</Label>
+        <SwitchChange
+          thumbColor="#FFF"
+          trackColor={{ false: '#ccc', true: '#39cc83' }}
+        />
+      </SwitchContainer>
+
+      <NextButton onPress={() => {}}>
+        <NextButtonText>Cadastrar</NextButtonText>
+      </NextButton>
     </Container>
   );
 };
