06686512d6af86e2c2c25a01a59ebe75afca52b4
ialexanderbrito <xanderzinho26@gmail.com>
2022-01-25 17:34:21 -0300

    deploy

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import 'express-async-errors';
+
+import './database/connection';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log('🚀 Backend started!');
+});
