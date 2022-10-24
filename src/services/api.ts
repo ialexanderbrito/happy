eaeaee6660f45cc09ac15776efc92c0d42cca6a2
Alexander <xanderzinho26@gmail.com>
2022-10-24 16:25:30 -0300

    fix: link api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://happy-oficial.herokuapp.com',
+  baseURL: 'https://happy-api.ialexanderbrito.dev',
 });
 
 export default api;
